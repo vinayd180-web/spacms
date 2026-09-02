b7c211928a01c0ac7384acce0987c1c4871b184b
Vinay <vinayd180@example.com>
2026-09-02 13:43:53 +0530

    Use WhiteNoise storage

--- managementProject/settings.py
+++ managementProject/settings.py
@@ -166,7 +166,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
